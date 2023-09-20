841857a2b009475170789aafaa032424cb985b0f
Gabriel Rezende Machado <grm1003@gmail.com>
2023-09-20 19:28:15 -0300

    ajustes row

--- js/index.js
+++ js/index.js
@@ -1,53 +1,35 @@
-
-var email = document.getElementById('email').value;
-var senha = document.getElementById('senha').value;
-
 document.getElementById('login').addEventListener('submit', (e) => {
-
- 
-
-    if(!validateEmail()) {
-        e.preventDefault(); 
+    e.preventDefault();
+    var email = document.getElementById('email').value;
+    var senha = document.getElementById('senha').value;
+    if(email == '') {
         appendAlert("Email inválido!",'liveAlertPlaceholderEmail');
     }
-
-    if(!validateSenha()) {
-        e.preventDefault(); 
+    if(senha == '') {
         appendAlert( "Senha não contém 8 caracteres e um número!",'liveAlertPlaceholderSenha');
     }
 
-    debugger
-    if(!validateEmail() && !validateSenha())return;
+    if(email == '' || senha == '')
+        return;
     else{
-        debugger
-        console.log("tudo ok")
         document.getElementById('login').submit();
-
     }
-    
-        
-    
-    
 })
 
 
-function appendAlert(message,id){
-   
-    var alert = document.getElementById(id); 
-    const wrapper = document.createElement('div')
-  wrapper.innerHTML = [
-    `<div class="alert alert-warning alert-dismissible" role="alert">`,
-    `   <div>${message}</div>`,
-    '</div>'
-  ].join('')
-
-  alert.append(wrapper)
+function appendAlert(message,id) {
+    var alert = document.getElementById(id);
+    if (!alert.hasChildNodes()) {
+        const wrapper = document.createElement('div')
+      wrapper.innerHTML = [
+        `<div class="alert alert-warning alert-dismissible" role="alert">`,
+        `   <div>${message}</div>`,
+        '</div>'
+      ].join('')
+    
+      alert.append(wrapper)
+    }
 }
-
-
-
-
-
 
 function validateSenha() {
     return senha.length >= 8;
@@ -58,9 +40,3 @@ function validateEmail() {
     const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
     return emailPattern.test(email);
 }
-
-  
-
-    
-
-
